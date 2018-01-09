0bab33a89c650df22014e182fe9899c0b175b17a
Lan Pham <lanpham312@gmail.com>
2018-01-09 16:29:03 +0000

    commenting out server helper

--- server-helper.js
+++ server-helper.js
@@ -1,6 +1,6 @@
-var httpServer = require("http-server");
-var path = require("path");
-
-var pathToHtmlAndJsFiles = path.join(__dirname);
-var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
-server.listen(8080);
+// var httpServer = require("http-server");
+// var path = require("path");
+//
+// var pathToHtmlAndJsFiles = path.join(__dirname);
+// var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
+// server.listen(8080);
