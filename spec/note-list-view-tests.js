4a72afbcebb5385437e05b253aef9b9704cc9371
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-11 18:04:21 +0000

    PAssed tests for displaying only first 20 characters of each single note

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -2,12 +2,12 @@
 function testForReturnHTMLNote(){
   var list = new List();
   var view = new View(list);
-  var note = new Note("How are you?");
+  var note = new Note("123456789123456789123456789");
   list.addNote(note);
   var note2 = new Note("I'm good");
   list.addNote(note2);
 
-  
-  assert.isTrue(view.returnHTML() === "<ul><li>How are you?</li><li>I'm good</li></ul>", "Convert one note to a html");
+
+  assert.isTrue(view.returnHTML() === "<ul><li>12345678912345678912</li><li>I'm good</li></ul>", "Convert one note to a html");
 };
 testForReturnHTMLNote();
--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -7,10 +7,12 @@
   View.prototype.returnHTML = function () {
 ;   var text = []
     for (var i=0; i < this.list.notes.length; i ++){
-     text.push(this.list.notes[i].text)
+
+      text.push(this.list.notes[i].text.slice(0, 20))
    };
-  
+   console.log()
    return "<ul><li>" + text.join("</li><li>") + "</li></ul>"
+
   };
 
 
