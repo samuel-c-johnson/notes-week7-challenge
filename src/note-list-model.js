15cffe7dab86cc7033194331b44002766c7995c4
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-12 14:59:53 +0000

    Added a unique id for each new note

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -2,11 +2,20 @@
 
   function List() {
     this.notes = [];
+    this.id = 0
   }
 
   List.prototype.addNote = function(note){
     this.notes.push(note);
   }
+
+  // List.prototype.createNote = function(notetext){
+  //   note = new Note(notetext, this.id)
+  //   // list = new List
+  //   console.log(note)
+  //   List.addNote(note)
+  //   this.id += 1
+  // }
 
   exports.List = List;
 
--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -5,13 +5,14 @@
   }
 
   View.prototype.returnHTML = function () {
-;   var text = []
+    var text = []
     for (var i=0; i < this.list.notes.length; i ++){
 
       text.push(this.list.notes[i].text.slice(0, 20))
    };
    console.log()
    return "<ul><li>" + text.join("</li><li>") + "</li></ul>"
+
 
   };
 
